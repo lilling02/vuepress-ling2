f2aac40ab634dd378d6696f85d7ca7090ed27c93
Ling <oncwnuKKrlyyFDJA-9MbXDaHSl68@git.weixin.qq.com>
2023-02-09 14:54:34 +0800

    修改路径问题

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,5 +1,5 @@
 /**
- * 提示：如您想使用JS版本的配置文件可参考：https://github.com/xugaoyi/vuepress-theme-vdoing/tree/a2f03e993dd2f2a3afdc57cf72adfc6f1b6b0c32/docs/.vuepress
+ * 提示:如您想使用JS版本的配置文件可参考:https://github.com/xugaoyi/vuepress-theme-vdoing/tree/a2f03e993dd2f2a3afdc57cf72adfc6f1b6b0c32/docs/.vuepress
  */
 import { resolve } from 'path'
 import { defineConfig4CustomTheme, UserPlugins } from 'vuepress/config'
@@ -19,11 +19,11 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     '/': {
       lang: 'zh-CN',
       title: "Ling Blog",
-      description: 'web前端技术博客。JavaScript,js,ES6,TypeScript,vue,React,python,css3,html5,Node,git,github等技术文章。',
+      description: 'web前端技术博客.JavaScript,js,ES6,TypeScript,vue,React,python,css3,html5,Node,git,github等技术文章.',
     }
   },
-  base: '/vuepress-theme-vdoing/',
-  // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+  base: './',
+  // 默认'/'.如果你想将你的网站部署到如 https://foo.github.io/bar/,那么 base 应该被设置成 "/bar/",(否则页面将失去样式等文件)
 
   // 主题配置
   themeConfig: {
@@ -32,9 +32,9 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       { text: '首页', link: '/' },
       {
         text: '前端',
-        link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+        link: '/web/', //目录页链接,此处link是vdoing主题新增的配置项,有二级导航时,可以点击一级导航跳到目录页
         items: [
-          // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
+          // 说明:以下所有link的值只是在相应md文件头部定义的永久链接(不是什么特殊编码).另外,注意结尾是有斜杠的
           {
             text: '前端文章',
             items: [
@@ -44,17 +44,17 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           {
             text: '学习笔记',
             items: [
-              { text: '《JavaScript教程》', link: '/note/javascript/' },
-              { text: '《JavaScript高级程序设计》', link: '/note/js/' },
-              { text: '《ES6 教程》', link: '/note/es6/' },
-              { text: '《Vue》', link: '/note/vue/' },
-              { text: '《React》', link: '/note/react/' },
+              { text: '<JavaScript教程>', link: '/note/javascript/' },
+              { text: '<JavaScript高级程序设计>', link: '/note/js/' },
+              { text: '<ES6 教程>', link: '/note/es6/' },
+              { text: '<Vue>', link: '/note/vue/' },
+              { text: '<React>', link: '/note/react/' },
               {
-                text: '《TypeScript 从零实现 axios》',
+                text: '<TypeScript 从零实现 axios>',
                 link: '/note/typescript-axios/',
               },
               {
-                text: '《Git》',
+                text: '<Git>',
                 link: '/note/git/',
               },
               {
@@ -80,54 +80,54 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         ],
       },
     ],
-    sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
+    sidebarDepth: 2, // 侧边栏显示深度,默认1,最大2(显示到h3标题)
     logo: '/img/logo.png', // 导航栏logo
     repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
-    lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
+    lastUpdated: '上次更新', // 开启更新时间,并配置前缀文字   string | boolean (取值为git提交时间)
     docsDir: 'docs', // 编辑的文件夹
-    // docsBranch: 'master', // 编辑的文件所在分支，默认master。 注意：如果你的分支是main则修改为main
+    // docsBranch: 'master', // 编辑的文件所在分支,默认master. 注意:如果你的分支是main则修改为main
     editLinks: true, // 启用编辑
     editLinkText: '编辑',
 
-    //*** 以下是Vdoing主题相关配置，文档：https://doc.xugaoyi.com/pages/a20ce8/ ***//
+    //*** 以下是Vdoing主题相关配置,文档:https://doc.xugaoyi.com/pages/a20ce8/ ***//
 
-    // category: false, // 是否打开分类功能，默认true
-    // tag: false, // 是否打开标签功能，默认true
-    // archive: false, // 是否打开归档功能，默认true
-    // categoryText: '随笔', // 碎片化文章（_posts文件夹的文章）预设生成的分类值，默认'随笔'
+    // category: false, // 是否打开分类功能,默认true
+    // tag: false, // 是否打开标签功能,默认true
+    // archive: false, // 是否打开归档功能,默认true
+    // categoryText: '随笔', // 碎片化文章(_posts文件夹的文章)预设生成的分类值,默认'随笔'
 
-    // pageStyle: 'line', // 页面风格，可选值：'card'卡片 | 'line' 线（未设置bodyBgImg时才生效）， 默认'card'。 说明：card时背景显示灰色衬托出卡片样式，line时背景显示纯色，并且部分模块带线条边框
+    // pageStyle: 'line', // 页面风格,可选值:'card'卡片 | 'line' 线(未设置bodyBgImg时才生效), 默认'card'. 说明:card时背景显示灰色衬托出卡片样式,line时背景显示纯色,并且部分模块带线条边框
 
     // bodyBgImg: [
     //   'https://fastly.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175828.jpeg',
     //   'https://fastly.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175845.jpeg',
     //   'https://fastly.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175846.jpeg'
-    // ], // body背景大图，默认无。 单张图片 String | 多张图片 Array, 多张图片时隔bodyBgImgInterval切换一张。
-    // bodyBgImgOpacity: 0.5, // body背景图透明度，选值 0.1~1.0, 默认0.5
-    // bodyBgImgInterval: 15, // body多张背景图时的切换间隔, 默认15，单位s
-    // titleBadge: false, // 文章标题前的图标是否显示，默认true
-    // titleBadgeIcons: [ // 文章标题前图标的地址，默认主题内置图标
+    // ], // body背景大图,默认无. 单张图片 String | 多张图片 Array, 多张图片时隔bodyBgImgInterval切换一张.
+    // bodyBgImgOpacity: 0.5, // body背景图透明度,选值 0.1~1.0, 默认0.5
+    // bodyBgImgInterval: 15, // body多张背景图时的切换间隔, 默认15,单位s
+    // titleBadge: false, // 文章标题前的图标是否显示,默认true
+    // titleBadgeIcons: [ // 文章标题前图标的地址,默认主题内置图标
     //   '图标地址1',
     //   '图标地址2'
     // ],
-    // contentBgStyle: 1, // 文章内容块的背景风格，默认无. 1 方格 | 2 横线 | 3 竖线 | 4 左斜线 | 5 右斜线 | 6 点状
+    // contentBgStyle: 1, // 文章内容块的背景风格,默认无. 1 方格 | 2 横线 | 3 竖线 | 4 左斜线 | 5 右斜线 | 6 点状
 
     // updateBar: { // 最近更新栏
-    //   showToArticle: true, // 显示到文章页底部，默认true
-    //   moreArticle: '/archives' // “更多文章”跳转的页面，默认'/archives'
+    //   showToArticle: true, // 显示到文章页底部,默认true
+    //   moreArticle: '/archives' // "更多文章"跳转的页面,默认'/archives'
     // },
-    // rightMenuBar: false, // 是否显示右侧文章大纲栏，默认true (屏宽小于1300px下无论如何都不显示)
-    // sidebarOpen: false, // 初始状态是否打开左侧边栏，默认true
-    // pageButton: false, // 是否显示快捷翻页按钮，默认true
+    // rightMenuBar: false, // 是否显示右侧文章大纲栏,默认true (屏宽小于1300px下无论如何都不显示)
+    // sidebarOpen: false, // 初始状态是否打开左侧边栏,默认true
+    // pageButton: false, // 是否显示快捷翻页按钮,默认true
 
-    // 默认外观模式（用户未在页面手动修改过模式时才生效，否则以用户设置的模式为准），可选：'auto' | 'light' | 'dark' | 'read'，默认'auto'。
+    // 默认外观模式(用户未在页面手动修改过模式时才生效,否则以用户设置的模式为准),可选:'auto' | 'light' | 'dark' | 'read',默认'auto'.
     // defaultMode: 'auto',
 
-    // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | <自定义>    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
+    // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | <自定义>    温馨提示:目录页数据依赖于结构化的侧边栏数据,如果你不设置为'structuring',将无法使用目录页
     sidebar: 'structuring',
 
-    // 文章默认的作者信息，(可在md文件中单独配置此信息) string | {name: string, link?: string}
+    // 文章默认的作者信息,(可在md文件中单独配置此信息) string | {name: string, link?: string}
     author: {
       name: 'ling huanliang', // 必需
       link: 'https://github.com/lilling02', // 可选的
@@ -140,9 +140,9 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       slogan: '前端小白',
     },
 
-    // 社交图标 (显示于博主信息栏和页脚栏。内置图标：https://doc.xugaoyi.com/pages/a20ce8/#social)
+    // 社交图标 (显示于博主信息栏和页脚栏.内置图标:https://doc.xugaoyi.com/pages/a20ce8/#social)
     social: {
-      // iconfontCssFile: '//at.alicdn.com/t/xxx.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自己添加。阿里图片库：https://www.iconfont.cn/
+      // iconfontCssFile: '//at.alicdn.com/t/xxx.css', // 可选,阿里图标库在线css文件地址,对于主题没有的图标可自己添加.阿里图片库:https://www.iconfont.cn/
       icons: [
         {
           iconClass: 'icon-youjian',
@@ -161,10 +161,10 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     footer: {
       createYear: 2022, // 博客创建年份
       copyrightInfo:
-        'Evan Xu | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息、备案信息等，支持a标签或换行标签</br>
+        'Evan Xu | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息、备案信息等,支持a标签或换行标签</br>
     },
 
-    // 扩展自动生成frontmatter。（当md文件的frontmatter不存在相应的字段时将自动添加。不会覆盖已有的数据。）
+    // 扩展自动生成frontmatter.(当md文件的frontmatter不存在相应的字段时将自动添加.不会覆盖已有的数据.)
     extendFrontmatter: {
       author: {
         name: 'Ling HuanLiang',
@@ -176,9 +176,9 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     htmlModules
   },
 
-  // 注入到页面<head>中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
+  // 注入到页面<head>中的标签,格式[tagName, { attrName: attrValue }, innerHTML?]
   head: [
-    ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
+    ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons,资源放在public文件夹
     [
       'meta',
       {
@@ -186,7 +186,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         content: '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown',
       },
     ],
-    ['meta', { name: 'baidu-site-verification', content: '7F55weZDDc' }], // 百度统计的站长验证（你可以去掉）
+    ['meta', { name: 'baidu-site-verification', content: '7F55weZDDc' }], // 百度统计的站长验证(你可以去掉)
     ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
     // [
     //   'script',
@@ -195,7 +195,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     //     async: 'async',
     //     src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
     //   },
-    // ], // 网站关联Google AdSense 与 html格式广告支持（你可以去掉）
+    // ], // 网站关联Google AdSense 与 html格式广告支持(你可以去掉)
   ],
 
 
@@ -217,10 +217,10 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       },
     ],
 
-    // 全文搜索。 ⚠️注意：此插件会在打开网站时多加载部分js文件用于搜索，导致初次访问网站变慢。如在意初次访问速度的话可以不使用此插件！（推荐：vuepress-plugin-thirdparty-search）
+    // 全文搜索. ⚠️注意:此插件会在打开网站时多加载部分js文件用于搜索,导致初次访问网站变慢.如在意初次访问速度的话可以不使用此插件!(推荐:vuepress-plugin-thirdparty-search)
     // 'fulltext-search',
 
-    // 可以添加第三方搜索链接的搜索框（继承原官方搜索框的配置参数）
+    // 可以添加第三方搜索链接的搜索框(继承原官方搜索框的配置参数)
     [
       'thirdparty-search',
       {
@@ -228,7 +228,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           {
             title: '在MDN中搜索',
             frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-            behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
+            behindUrl: '', // 搜索链接的后面部分,可选,默认 ''
           },
           {
             title: '在Runoob中搜索',
@@ -298,7 +298,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
           labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
           body:
-            '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
+            '页面:<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
         },
       },
     ],
@@ -314,7 +314,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
 
   markdown: {
     lineNumbers: true,
-    extractHeaders: ['h2', 'h3', 'h4', 'h5', 'h6'], // 提取标题到侧边栏的级别，默认['h2', 'h3']
+    extractHeaders: ['h2', 'h3', 'h4', 'h5', 'h6'], // 提取标题到侧边栏的级别,默认['h2', 'h3']
   },
 
   // 监听文件变化并重新构建
